d5a107d61cfe6b94109a6967cbce0ef0f7f80e4f
Md Mahfuzul Hoque <mahfuzul.hoque666@gmail.com>
2026-04-23 16:32:24 +0600

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:crafty_bay/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'app/crafty_bay_app.dart';
+
+// Firebase set up
+// Crashlytics set up
+// Analytics set up
+// Provider set up
+// Localization set up
+// Architecture Design
+// Theming set up
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Pass all uncaught "fatal" errors from the framework to Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
+  runApp(const CraftyBayApp());
+}
+
+
+// Class -> api call -> ApiCaller -> NetworkCaller
